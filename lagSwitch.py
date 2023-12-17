3036c3c28ed16cb5a88afdd1c8639aadd7582e2d
Developer iX7 <78102400+DEVIX7@users.noreply.github.com>
2023-12-17 13:48:36 +0200

    v1.5

--- lagSwitch.py
+++ lagSwitch.py
@@ -1,33 +1,35 @@
 import keyboard
 import os
+from colorama import Fore, Style, init
 
-def tutor():
+init(autoreset=True)
+
+def main_window():
     os.system('cls')
-    print("Lag switch")
+    print(f"{Fore.CYAN}Lag switch v1.5")
     print("----------------------------------------")
-    print("     Ctrl + E = Internet enabled")
-    print("     Ctrl + Q = Internet disabled")
+    print(f"{Fore.YELLOW}\tCtrl + E{Style.RESET_ALL} = Internet enabled")
+    print(f"{Fore.YELLOW}\tCtrl + Q{Style.RESET_ALL} = Internet disabled")
+    #print("\tShift + Esc = Close Lag switch")
     print("----------------------------------------")
-    print("by devix7")
-    print(" ")
+    print(f"by {Fore.MAGENTA}devix7{Style.RESET_ALL}\n")
 
 def disable_internet():
-    tutor()
-    print('Status:')
+    main_window()
+    print(Fore.RED + 'Status:')
     print('Disabling internet now on Windows...')
     os.system('ipconfig/release > nul')
     print('Internet disabled.')
 
 def enable_internet():
-    tutor()
-    print('Status:')
+    main_window()
+    print(Fore.GREEN + 'Status:')
     print('Enabling internet now on Windows...')
     os.system('ipconfig/renew > nul')
     print('Internet enabled.')
 
-tutor()
+main_window()
 
 keyboard.add_hotkey('ctrl+q', disable_internet)
 keyboard.add_hotkey('ctrl+e', enable_internet)
-
-keyboard.wait('esc')
+keyboard.wait("left shift+esc")
